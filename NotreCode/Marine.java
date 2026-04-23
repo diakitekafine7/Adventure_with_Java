8d5119216e15f1e877f9448bdc8cba7e0675acec
InesAddam <inesaddam06@gmail.com>
2026-04-24 01:14:05 +0200

    Refactor Marine class to extend Agent and be abstract
    Changed Marine class to be abstract and extended from Agent.

--- NotreCode/Marine.java
+++ NotreCode/Marine.java
@@ -1,3 +1,12 @@
-public class Marine {
-    
+public abstract class Marine extends Agent {
+    protected String grade;
+    protected String nom;
+
+    public Marine(String nom, String grade, int lig, int col, Terrain t) {
+        super(lig, col, t); 
+        this.nom = nom;
+        this.grade = grade;
+    }
+
+    // On peut ajouter une méthode pour capturer un pirate apres
 }
