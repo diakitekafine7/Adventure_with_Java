06089e0c5ebe1322647aa9a3214d4889490d9b76
Kafine Diakite <diakitekafine@gmail.com>
2026-04-29 20:35:40 +0200

    modi nom UniteMarine

--- NotreCode/UniteMarine.java
+++ NotreCode/UniteMarine.java
@@ -1,8 +1,8 @@
-public abstract class UnitéMarine extends Agent implements Combattant{
+public abstract class UniteMarine extends Agent implements Combattant{
     protected String grade;
     protected String nom;
 
-    public UnitéMarine(String nom, String grade, int lig, int col, Terrain t) {
+    public UniteMarine(String nom, String grade, int lig, int col, Terrain t) {
         super(lig, col, t); 
         this.nom = nom;
         this.grade = grade;
